806092d8165c4adefc5307fe2bb2ba165c88eded
newtonis <newtonis.penguin@gmail.com>
2017-09-16 19:57:23 -0300

    added worm

---
+++ Worms/Worms/worm.cpp
@@ -0,0 +1,52 @@
+#include "worm.h"
+
+enum {IDLE , MONITOR_MOVING , MOVING , END_MOVEMENT , JUMPING };
+
+Worm::Worm(double _x, double _y, int _sentido,Physics physics) {
+	frame_count = 0;
+	this->x = _x;
+	this->y = _y;
+	this->sentido = _sentido;
+	this->state = IDLE;
+	this->physics = physics;
+	this->error = 0;
+}
+
+void Worm::start_moving(int sentido) {
+	if (this->state == IDLE) {
+		this->state = MONITOR_MOVING;
+		this->sentido = sentido;
+	}else if (this->state = END_MOVEMENT) {
+		this->state = MOVING;
+	}else {
+		this->error = 1; // this call is strange
+	}
+}
+void Worm::stop_moving() {
+	if (this->state == MOVING) {
+		this->state = END_MOVEMENT;
+	}else if (this->state == MONITOR_MOVING) {
+		this->state = IDLE;
+	}else {
+		this->error = 1;
+	}
+}
+void Worm::start_jumping() {
+	if (this->state == IDLE) {
+		this->state = JUMPING;
+	}else {
+		this->error = 1;
+	}
+}
+void Worm::stop_jumping() {
+	if (this->state == JUMPING) {
+		this->state = IDLE;
+	}
+	else {
+		this->error = 1;
+	}
+}
+
+void Worm::update() {
+	switch (this->state)
+}
--- Worms/Worms/worm.h
+++ Worms/Worms/worm.h
@@ -1,14 +1,33 @@
 #ifndef WORM_H
 #define WORM_H
 
+struct Pos {
+	double x, y;
+};
+
+struct Physics {
+	float speed; // speed
+	float gravity;
+	float angle;
+	Pos max_coordinates , min_coordinates;
+};
 
 class Worm {
 	private:
-		int x, y; 
+		float x, y;
 		int sentido;
 		int frame_count;
 		int state;
-	
+		int error;
+		Physics physics;
+	public:
+		Worm(double _x, double _y, int _sentido , Physics _physics);
+
+		void start_moving(int sentido);
+		void stop_moving();
+		void start_jumping();
+		void stop_jumping();
+		void update();
 };
 
 #endif //WORM_H
