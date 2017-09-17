abc6fb6d0f6e38ce01602e6f4074fce396dadb96
newtonis <newtonis.penguin@gmail.com>
2017-09-16 23:39:00 -0300

    9-16

--- Worms/Worms/worm.cpp
+++ Worms/Worms/worm.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
+#include <cmath>
 #include "worm.h"
-#include "config.h"
+using namespace std;
 
-enum {IDLE , MONITOR_MOVING , MOVING , END_MOVEMENT , JUMPING };
 
-Worm::Worm(double _x, double _y, int _sentido,Physics physics) {
+/// Internal
+float degcos(float angle);
+float degsin(float angle);
+
+float degcos(float angle) {
+	return (float)cos(angle * acos(0) * 2.0 / 180.0);
+}
+float degsin(float angle) {
+	return (float)sin(angle * acos(0) * 2.0 / 180.0);
+}
+
+Worm::Worm(double _x, double _y, int _sentido,Physics physics , int move_stage_period) {
 	frame_count = 0;
 	this->pos.x = _x;
 	this->pos.y = _y;
@@ -11,62 +23,118 @@ Worm::Worm(double _x, double _y, int _sentido,Physics physics) {
 	this->state = IDLE;
 	this->physics = physics;
 	this->error = 0;
+	this->move_stage_period = move_stage_period;
 }
 
 void Worm::start_moving(int sentido) {
-	if (sentido == -1 || sentido == 1) {
+	if (sentido != -1 && sentido != 1) {
 		this->error = 1; // this makes no sense
 		return; 
 	}
-
+	
 	if (this->state == IDLE) {
 		this->state = MONITOR_MOVING;
 		this->sentido = sentido;
 		this->frame_count = 0;
-	}else if (this->state = END_MOVEMENT) {
+	}else if (this->state == END_MOVEMENT) {
 		this->state = MOVING;
-	}else {
-		this->error = 1; // this call is strange
 	}
 }
 void Worm::stop_moving() {
 	if (this->state == MOVING) {
+
 		this->state = END_MOVEMENT;
 	}else if (this->state == MONITOR_MOVING) {
 		this->state = IDLE;
-	}else {
-		this->error = 1;
 	}
 }
 void Worm::start_jumping() {
+	
 	if (this->state == IDLE) {
-		this->state = JUMPING;
+		//cout << "jumping called \n";
+		this->state = MONITOR_JUMPING;
 		this->frame_count = 0;
-	}else {
-		this->error = 1;
+		this->update_jump_period();
 	}
 }
 void Worm::stop_jumping() {
-	if (this->state == JUMPING) {
+	if (this->state == MONITOR_JUMPING) {
 		this->state = IDLE;
 	}else {
 		this->error = 1;
 	}
 }
 
+void Worm::update_jump_period() {
+	float a = this->physics.jump_angle;
+	float v0 = this->physics.jump_speed;
+	float t = this->frame_count * this->physics.TIME_PER_UPDATE;
+	float g = this->physics.gravity;
+
+	//cout << v0 << ' ' << degsin(a) << '\n';
+	float jump_time = 2 * v0 * degsin(a) / (g);
+
+	this->jump_stage_period = (int)ceil(jump_time / this->physics.TIME_PER_UPDATE);
+	//cout << jump_time << '\n';
+	//cout << jump_time / 0.02 << '\n';
+	//cout << this->jump_stage_period << '\n';
+}
 void Worm::update() {
+	float t;
+
 	switch (this->state) {
 		case MONITOR_MOVING:
-			if (this->frame_count >= 5) {
+			if (this->frame_count*this->physics.TIME_PER_UPDATE >= 0.1) {
+				this->original_pos = this->pos;
 				this->state = MOVING;
+				this->frame_count = 0;
 			}
 		break;
+		case MONITOR_JUMPING:
+			if (this->frame_count*this->physics.TIME_PER_UPDATE >= 0.1) {
+				
+				this->state = JUMPING;
+				this->original_pos = this->pos;
+				this->frame_count = 0;
+			}
+			break;
 		case MOVING:
-			this->pos.x += this->sentido * WORM_MOVEMENT_FACTOR;
+		case END_MOVEMENT:
+			t = this->frame_count * this->physics.TIME_PER_UPDATE;
+
+			this->pos.x = this->original_pos.x + this->sentido * this->physics.speed * t;
+			this->move_stage = this->frame_count % this->move_stage_period;
+
 			this->correct_range();
+			if (this->state == END_MOVEMENT && this->frame_count % 50 == 0) {
+				this->state = IDLE;
+				this->move_stage = 0;
+			}
 		break;
+	
 		case JUMPING:
 
+			float a = this->physics.jump_angle;
+			float v0 = this->physics.jump_speed;
+			t = this->frame_count * this->physics.TIME_PER_UPDATE;
+			
+			float g = this->physics.gravity;
+
+			double mruv_y = v0 * degsin(a) * t - g*t*t / 2.0;
+			double mruv_x = this->sentido * v0 * degcos(a) * t;
+			//cout << "v0 = " << degsin(a) << '\n';
+
+			//cout << mruv_x << '\n';
+
+			this->pos.y = this->original_pos.y - mruv_y;
+			this->pos.x = this->original_pos.x + mruv_x; ;
+
+			this->correct_range();
+
+			this->jump_stage = this->frame_count;
+			if (this->jump_stage == this->jump_stage_period) {
+				this->state = IDLE;
+			}
 		break;
 	}
 	this->frame_count++;
@@ -93,4 +161,14 @@ int Worm::get_state() {
 }
 Pos Worm::get_position() {
 	return this->pos;
+}
+
+int Worm::get_jump_stage_animation() {
+	return this->jump_stage;
+}
+int Worm::get_move_stage_animation() {
+	return this->move_stage;
+}
+int Worm::get_if_error() {
+	return this->error;
 }
--- Worms/Worms/worm.h
+++ Worms/Worms/worm.h
@@ -1,7 +1,10 @@
 #ifndef WORM_H
 #define WORM_H
 
-/// We assume frame counts increments each 20 ms
+
+enum { IDLE, MONITOR_MOVING, MOVING, END_MOVEMENT, JUMPING, MONITOR_JUMPING };
+
+/// We as
 
 struct Pos {
 	double x, y;
@@ -9,34 +12,47 @@ struct Pos {
 
 struct Physics {
 	float speed; // speed
+	float jump_speed;
+	float jump_angle;
 	float gravity;
-	float angle;
 	Pos max_coordinates , min_coordinates;
 
+	float TIME_PER_UPDATE;
 };
 
 class Worm {
 	private:
-		Pos pos;
+		Pos pos , original_pos;
 		int sentido;
 		int frame_count;
 		int state;
 		int error;
 		Physics physics;
 
+		int move_stage;
+		int jump_stage;
+
+		int jump_stage_period;
+		int move_stage_period;
+
 		void correct_range();
+		void update_jump_period();
 	public:
-		Worm(double _x, double _y, int _sentido , Physics _physics);
+		Worm(double _x, double _y, int _sentido, Physics physics, int move_stage_period);
 
 		void start_moving(int sentido);
 		void stop_moving();
 		void start_jumping();
 		void stop_jumping();
+
 		void update();
 
 		int get_sentido(); // 1 o -1
 		int get_state();
 		Pos get_position();
+		int get_jump_stage_animation();
+		int get_move_stage_animation();
+		int get_if_error();
 };
 
 #endif //WORM_H
